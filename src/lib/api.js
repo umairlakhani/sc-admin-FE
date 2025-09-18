1016b54502181be6ec01216006005c33b88701b9
hunesh kumar <huneshk@gmail.com>
2025-09-18 11:38:32 +0500

    uncommit backend url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://api.searchcasa.ch'
-const baseURL = "http://localhost:3000";
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://api.searchcasa.ch'
+//const baseURL = "http://localhost:3000";
 export const api = axios.create({ baseURL, withCredentials: false });
 
 // Attach Authorization header if token is present
